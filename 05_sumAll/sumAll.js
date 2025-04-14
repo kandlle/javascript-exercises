984f98b30290d0c63bed4a0c316a417ac21e180e
kandlle <140917689+kandlle@users.noreply.github.com>
2025-04-14 01:52:34 -0400

    yippee!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }    
+    let sum = 0;
+    if (start > end) {
+        for (let i = end; i <= start; i++) {
+            sum += i;
+        }
+    }
+    if (start < end) {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
