85b57601f748378b114941c3d800922c3fd27d42
kandlle <140917689+kandlle@users.noreply.github.com>
2025-04-14 02:25:57 -0400

    yowza

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  let rawC = ((F - 32) * 5)/9;
+  return Math.round(rawC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let rawF = ((C * 9)/5) + 32;
+  return Math.round(rawF * 10) / 10;
 };
 
 // Do not edit below this line
